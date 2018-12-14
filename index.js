cc23b28a1eabb58d9facd343a3c25ab068ff1a85
Samidh Patel <spatel13456@gmail.com>
2018-12-14 20:48:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay (arrayMusicians, arrayInstruments) {
+  var array = [];
+  for (let i = 0; i<arrayMusicians.length; i++) {
+        array.push(arrayMusicians[i] + " plays " + arrayInstruments[i]);
+      }
+      return array;
+    }
+    
+    function johnLennonFacts(arrayFacts) {
+      while (arrayFacts > 0) {
+        arrayFacts++
+      }
+      return arrayFacts + "!!!"
+    }
+    
+    
+  
